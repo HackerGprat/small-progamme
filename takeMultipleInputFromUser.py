324ac62c22542da10e7c176b78378046fe74c2f5
HackerGprat <41012110+GPratyush01@users.noreply.github.com>
2020-10-05 18:49:10 +0530

    how to take multiple inputs from users

---
+++ takeMultipleInputFromUser.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# x,y = str(input("Enter A Two Value: ")).split()
+
+# print("First number is : ", x)
+# print("Second number is : ", y)
+
+# a, b, c = input("inter dob : ").split()
+# print("your dob is : ",a,"here is b",b)
+
+# a, b = input("Enter two value: ").split()
+# print("First number is {} \n Second number is {}".format(a,b))
+
